172a6ca94844566f61a99808d187aa9a65cc0be3
Guillermo Cerviño Porto <144793938+GuillermoGCP@users.noreply.github.com>
2025-02-05 17:40:48 +0100

    Translation works, done

--- src/controllers/calendar/updateEventController.js
+++ src/controllers/calendar/updateEventController.js
@@ -1,120 +1,130 @@
 import {
-    updateEvent,
-    getEvent,
-    getRowsData,
-    updateRow,
+  updateEvent,
+  getEvent,
+  getRowsData,
+  updateRow,
 } from '../../googleapis/methods/index.js'
-import { filterProperties, formatDate } from '../../utils/index.js'
+import {
+  filterProperties,
+  formatDate,
+  translateTextWithPageBreak,
+} from '../../utils/index.js'
 import cloudinaryDelete from '../cloudinary/deleteImage.js'
 import cloudinaryUpload from '../cloudinary/uploadImage.js'
 
 const updateEventController = async (req, res, next) => {
-    try {
-        const eventId = req.params.eventId
-        const updatedData = req.body
-        const sheetId = process.env.SPREADSHEET_ID
-        let accessDataSheet
-        if (
-            req.body.extendedProperties &&
-            req.body.extendedProperties.private.access === 'free'
-        ) {
-            accessDataSheet = 'libre'
-        } else accessDataSheet = 'solo_socios'
+  try {
+    const eventId = req.params.eventId
+    const updatedData = req.body
+    const sheetId = process.env.SPREADSHEET_ID
+    let accessDataSheet
+    if (
+      req.body.extendedProperties &&
+      req.body.extendedProperties.private.access === 'free'
+    ) {
+      accessDataSheet = 'libre'
+    } else accessDataSheet = 'solo_socios'
 
-        //Traigo el evento del calendario:
-        const existingEvent = await getEvent(eventId)
+    //Traigo el evento del calendario:
+    const existingEvent = await getEvent(eventId)
 
-        //Compruebo la imagen y la actualizo, si es necesario:
-        let image = req.body.extendedProperties.private.image;
+    //Compruebo la imagen y la actualizo, si es necesario:
+    let image = req.body.extendedProperties.private.image
 
-        if (req.file) {
-            const imageUrl = await cloudinaryUpload(req.file, 'calendarEvents')
-            image = imageUrl || 'sin imagen'
-            await cloudinaryDelete(
-                req.body.extendedProperties.private.image
-            )
-        }
-
-        const allowedProperties = [
-            'summary',
-            'location',
-            'description',
-            'start',
-            'end',
-            'location',
-            'attendees',
-            'reminders',
-            'visibility',
-            'access',
-            'extendedProperties',
-        ]
-
-        //Creo un objeto solo con las propiedades que admite el método update de calendar:
-        const filteredExistingEvent = filterProperties(
-            existingEvent,
-            allowedProperties
-        )
-        const filteredUpdatedData = filterProperties(
-            updatedData,
-            allowedProperties
-        )
-
-        const mergedEvent = {
-            ...filteredExistingEvent,
-            ...filteredUpdatedData,
-            start: {
-                ...filteredExistingEvent.start,
-                ...filteredUpdatedData.start,
-            },
-            end: {
-                ...filteredExistingEvent.end,
-                ...filteredUpdatedData.end,
-            },
-            extendedProperties: {
-                private: {
-                    access: accessDataSheet,
-                    image,
-                },
-            },
-        }
-
-        //Lo actualizo:
-        const response = await updateEvent(eventId, mergedEvent)
-
-        //Actualizo el evento en la hoja de cálculo de Google:
-        const fields = {
-            field: 'id',
-            value: eventId,
-            newValue: '',
-            sheetName: 'Actividades',
-        }
-        let rowValuesToUpdate = [
-            eventId,
-            mergedEvent.summary,
-            mergedEvent.description,
-            formatDate(mergedEvent.start.dateTime),
-            formatDate(mergedEvent.end.dateTime),
-            mergedEvent.location,
-            mergedEvent.extendedProperties.private.access,
-            image,
-        ]
-        const dataToUpdate = await getRowsData(
-            sheetId,
-            'Actividades',
-            fields,
-            rowValuesToUpdate
-        )
-        const { rowToUpdate } = dataToUpdate
-        await updateRow(rowToUpdate)
-
-        res.send({
-            message: 'Evento actualizado',
-            data: response,
-        })
-    } catch (error) {
-        console.log(error)
-        next(error)
+    if (req.file) {
+      const imageUrl = await cloudinaryUpload(req.file, 'calendarEvents')
+      image = imageUrl || 'sin imagen'
+      await cloudinaryDelete(req.body.extendedProperties.private.image)
     }
+
+    const allowedProperties = [
+      'summary',
+      'location',
+      'description',
+      'start',
+      'end',
+      'location',
+      'attendees',
+      'reminders',
+      'visibility',
+      'access',
+      'extendedProperties',
+    ]
+
+    //Creo un objeto solo con las propiedades que admite el método update de calendar:
+    const filteredExistingEvent = filterProperties(
+      existingEvent,
+      allowedProperties
+    )
+    const filteredUpdatedData = filterProperties(updatedData, allowedProperties)
+
+    const mergedEvent = {
+      ...filteredExistingEvent,
+      ...filteredUpdatedData,
+      start: {
+        ...filteredExistingEvent.start,
+        ...filteredUpdatedData.start,
+      },
+      end: {
+        ...filteredExistingEvent.end,
+        ...filteredUpdatedData.end,
+      },
+      extendedProperties: {
+        private: {
+          access: accessDataSheet,
+          image,
+          glDescription: await translateTextWithPageBreak(
+            updatedData.description,
+            'gl'
+          ),
+          glSummary: await translateTextWithPageBreak(
+            updatedData.summary,
+            'gl'
+          ),
+        },
+      },
+    }
+
+    //Lo actualizo:
+    const response = await updateEvent(eventId, mergedEvent)
+
+    //Actualizo el evento en la hoja de cálculo de Google:
+    const fields = {
+      field: 'id',
+      value: eventId,
+      newValue: '',
+      sheetName: 'Actividades',
+    }
+    let rowValuesToUpdate = [
+      eventId,
+      mergedEvent.summary,
+      mergedEvent.description,
+      formatDate(mergedEvent.start.dateTime),
+      formatDate(mergedEvent.end.dateTime),
+      mergedEvent.location,
+      mergedEvent.extendedProperties.private.access,
+      'confirmed',
+      image,
+      mergedEvent.extendedProperties.private.glDescription,
+      mergedEvent.extendedProperties.private.glSummary,
+    ]
+    const dataToUpdate = await getRowsData(
+      sheetId,
+      'Actividades',
+      fields,
+      rowValuesToUpdate
+    )
+    const { rowToUpdate } = dataToUpdate
+    await updateRow(rowToUpdate)
+
+    res.send({
+      message: 'Evento actualizado',
+      data: response,
+    })
+  } catch (error) {
+    console.log(error)
+    next(error)
+  }
 }
 export default updateEventController
 
--- src/controllers/forms/unpublishForm.js
+++ src/controllers/forms/unpublishForm.js
@@ -1,18 +1,18 @@
-import Form from '../../Database/models/FormModel.js'
+import Form from '../../Database/models/FormModel2.js'
 
 const unpublishForm = async (req, res, next) => {
-    try {
-        const jsonNumber = req.params.jsonNumber.toString()
-        await Form.deleteOne({
-            publishNumber: jsonNumber,
-        })
+  try {
+    const jsonNumber = req.params.jsonNumber.toString()
+    await Form.deleteOne({
+      publishNumber: jsonNumber,
+    })
 
-        console.log('El formulario ha sido despublicado.')
+    console.log('El formulario ha sido despublicado.')
 
-        res.send({ message: 'Formulario despublicado', form: {} })
-    } catch (error) {
-        next(error)
-    }
+    res.send({ message: 'Formulario despublicado', form: {} })
+  } catch (error) {
+    next(error)
+  }
 }
 
 export default unpublishForm
